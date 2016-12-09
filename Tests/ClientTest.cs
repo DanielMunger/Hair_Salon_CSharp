579dda525b47141c91d2e60eb890f971e4e7b6ca
Daniel Munger <mungerda@gmail.com>
2016-12-08 21:54:06 -0800

    add test for empty table. passing

--- Tests/ClientTest.cs
+++ Tests/ClientTest.cs
@@ -1,17 +1,17 @@
-using Xunit;
-using System.Collections.Generic;
-using System;
-using System.Data;
-using System.Data.SqlClient;
-using HairSalon.Objects;
-
-namespace HairSalonTest
-{
-  public class ClientTest : IDisposable
-  {
-    public ClientTest()
-    {
-      DBConfiguration.ConnectionString = "Data Source=DESKTOP-GC3DC7B\\SQLEXPRESS;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
-    }
-  }
-}
+// using Xunit;
+// using System.Collections.Generic;
+// using System;
+// using System.Data;
+// using System.Data.SqlClient;
+// using HairSalon.Objects;
+//
+// namespace HairSalonTest
+// {
+//   public class ClientTest : IDisposable
+//   {
+//     public ClientTest()
+//     {
+//       DBConfiguration.ConnectionString = "Data Source=DESKTOP-GC3DC7B\\SQLEXPRESS;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+//     }
+//   }
+// }
--- Tests/StylistTest.cs
+++ Tests/StylistTest.cs
@@ -5,13 +5,22 @@ using System.Data;
 using System.Data.SqlClient;
 using HairSalon.Objects;
 
-namespace HairSalonTest
+
+namespace HairSalon.Test
 {
-  public class StylistTest : IDisposable
+  public class StylistTest
   {
     public StylistTest()
     {
       DBConfiguration.ConnectionString = "Data Source=DESKTOP-GC3DC7B\\SQLEXPRESS;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+    }
+    [Fact]
+    public void Test_StylistTableEmpty_True()
+    {
+      //Arrange,Act
+      int result = Stylist.GetAll().Count;
+      //Assert
+      Assert.Equal(0,result);
     }
   }
 }
