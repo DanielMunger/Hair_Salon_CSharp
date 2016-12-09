035c7ee2f8ecdfc4e7f53d887ea9f76ef2dc79b9
Daniel Munger <mungerda@gmail.com>
2016-12-09 10:36:37 -0800

    add database test for Client.

--- Tests/ClientTest.cs
+++ Tests/ClientTest.cs
@@ -7,11 +7,17 @@ using HairSalon.Objects;
 
 namespace HairSalon.Test
 {
-  public class ClientTest : IDisposable
+  public class ClientTest
   {
     public ClientTest()
     {
       DBConfiguration.ConnectionString = "Data Source=DESKTOP-GC3DC7B\\SQLEXPRESS;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+    }
+    [Fact]
+    public void Test_DatabaseEmpty()
+    {
+      int result = Client.GetAll().Count;
+      Assert.Equal(0,result);
     }
   }
 }
