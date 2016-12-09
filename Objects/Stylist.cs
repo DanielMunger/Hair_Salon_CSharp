35fca6fe9e769439bbef4297afbe1c781edb35c7
Daniel Munger <mungerda@gmail.com>
2016-12-09 14:24:23 -0800

    add getclient method to stylist class.

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -183,6 +183,35 @@ namespace HairSalon.Objects
         conn.Close();
       }
     }
+    public List<Client> GetClients()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("SELECT * FROM clients WHERE stylist_id = @StylistId;", conn);
+      SqlParameter stylistIdParameter = new SqlParameter("@StylistId", this.GetId());
+      cmd.Parameters.Add(stylistIdParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      List<Client> clients = new List<Client>{};
+
+      while(rdr.Read())
+      {
+        int clientId = rdr.GetInt32(0);
+        string clientName = rdr.GetString(1);
+        int stylistId = rdr.GetInt32(2);
+        Client newClient = new Client(clientName, stylistId, clientId);
+        clients.Add(newClient);
+      }
+      if(rdr!=null)
+      {
+        rdr.Close();
+      }
+      if(conn!=null)
+      {
+        conn.Close();
+      }
+      return clients;
+    }
     public void Delete()
     {
       SqlConnection conn = DB.Connection();
