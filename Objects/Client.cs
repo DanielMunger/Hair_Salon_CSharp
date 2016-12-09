debd8cd958a2b65bc5ce0cb6bccc582074834443
Daniel Munger <mungerda@gmail.com>
2016-12-09 10:51:27 -0800

    add getall method for Client class.

--- Objects/Client.cs
+++ Objects/Client.cs
@@ -6,6 +6,63 @@ namespace HairSalon.Objects
 {
   public class Client
   {
+    private int _id;
+    private string _clientName;
+    private int _stylistId;
+
+    public Client(string ClientName, int StylistId, int Id = 0)
+    {
+      _id = Id;
+      _clientName = ClientName;
+      _stylistId = StylistId;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public void SetClientName(string ClientName)
+    {
+      _clientName = ClientName;
+    }
+    public string GetClientName()
+    {
+      return _clientName;
+    }
+    public void SetStylistId(int StylistId)
+    {
+      _stylistId = StylistId;
+    }
+    public int GetStylistId()
+    {
+      return _stylistId;
+    }
+    public static List<Client> GetAll()
+    {
+      List<Client> allClients = new List<Client> {};
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM clients;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int clientId = rdr.GetInt32(0);
+        string clientName = rdr.GetString(1);
+        int stylistId = rdr.GetInt32(2);
+        Client newClient = new Client(clientName, stylistId, clientId);
+        allClients.Add(newClient);
+      }
+      if(rdr!=null)
+      {
+        rdr.Close();
+      }
+      if(conn!=null)
+      {
+        conn.Close();
+      }
+      return allClients;
+    }
 
   }
 
